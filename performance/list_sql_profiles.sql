4058c04eaf207a944d5867533b8ec693017ffda5
rvieira <ricardo@rvieira.co.uk>
2022-07-14 16:38:18 +0200

    formatting sql_profiles report

--- performance/list_sql_profiles.sql
+++ performance/list_sql_profiles.sql
@@ -1,4 +1,6 @@
-col category for a10
-col sql_text for a20
+set lines 300 pages 100
+col category for a20
+col sql_text for a40
+col sql_text for a100
 
 select name,sql_text,category,status from dba_sql_profiles;
