2c41d45fad5453bc8129e9807a1761828d6d04a4
Ricardo Augusto Ripoll Vieira <ricardo@rvieira.co.uk>
2022-11-18 11:10:12 +0100

    modified:   dp/correct_mount_options_for_nfs_share.sh

--- dp/correct_mount_options_for_nfs_share.sh
+++ dp/correct_mount_options_for_nfs_share.sh
@@ -1,2 +1,3 @@
 # Correct mount options for nfs share for data pump export on 11g
+# ORA-27054: NFS File System Where the File is Created or Resides is Not Mounted With Correct Options (Doc ID 781349.1)
 mount -F nfs -o rw,bg,hard,rsize=32768,wsize=32768,vers=3,forcedirectio,nointr,proto=tcp,suid host:/folder1/to1 /folder2/to2
