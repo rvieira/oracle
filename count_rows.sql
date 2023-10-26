fe97582d92fc7c4b743bcc4db1285b59f5cd493b
Ricardo Vieira <ricardo@rvieira.co.uk>
2023-10-26 11:37:25 +0200

    Update count_rows.sql

--- count_rows.sql
+++ count_rows.sql
@@ -1,1 +1,9 @@
-786738
+DECLARE 
+val NUMBER;
+BEGIN
+FOR I IN (SELECT TABLE_NAME FROM USER_TABLES) LOOP
+EXECUTE IMMEDIATE 'SELECT count(*) FROM ' || i.table_name INTO val;
+DBMS_OUTPUT.PUT_LINE(i.table_name || ' ==> ' || val );
+END LOOP;
+END;
+/
