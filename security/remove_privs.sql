58adb0877155d75d2f756c842d3a9b5d94d2ba47
Ricardo Augusto Ripoll Vieira <ricardo@rvieira.co.uk>
2025-01-09 13:44:11 +0100

    changing remove privileges

--- security/remove_privs.sql
+++ security/remove_privs.sql
@@ -1,8 +1,21 @@
+SET SERVEROUTPUT ON
 BEGIN
-    FOR rec IN (SELECT grantee, privilege 
-                FROM dba_tab_privs 
-                WHERE grantee = 'READ_ONLY_SCHEMA') LOOP
-        EXECUTE IMMEDIATE 'REVOKE ' || rec.privilege || ' FROM ' || rec.grantee;
+    FOR priv_rec IN (
+        SELECT privilege
+        FROM dba_tab_privs
+        WHERE grantee = 'SCHEMA_RO'
+        UNION
+        SELECT privilege
+        FROM dba_sys_privs
+        WHERE grantee = 'SCHEMA_RO' AND privilege != 'CREATE SESSION'
+        UNION
+        SELECT role AS privilege
+        FROM dba_role_privs
+        WHERE grantee = 'SCHEMA_RO'
+    )
+    LOOP
+        -- Generate REVOKE statements
+        DBMS_OUTPUT.PUT_LINE('REVOKE ' || priv_rec.privilege || ' FROM SCHEMA_RO;');
     END LOOP;
 END;
 /
